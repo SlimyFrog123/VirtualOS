98808205ca819fe7e8e39d55734a19f601c68057
SlimyFrog123 <danieldcookjr@gmail.com>
2022-10-25 20:30:19 -0700

    Updated `web` module, adding `wget` <url> <filepath> command.

--- modules/web_module.py
+++ modules/web_module.py
@@ -3,8 +3,10 @@
 ##############################
 
 
+import os
 import requests
-from vos_file_system import FileSystem
+import urllib.request
+from vos_file_system import FileSystem, Path
 
 
 ##############################
@@ -12,7 +14,7 @@ from vos_file_system import FileSystem
 ##############################
 
 
-def wprint_command(args: list, as_admin: bool, file_system: FileSystem) -> str:
+def wprint_command(args: list, as_admin: bool) -> str:
     if len(args) == 0:
         return 'Please specify a url to print from.'
     else:
@@ -30,6 +32,31 @@ def wprint_command(args: list, as_admin: bool, file_system: FileSystem) -> str:
             return 'Too many redirects.'
 
 
+def download(url: str, file: Path):
+    try:
+        urllib.request.urlretrieve(url, file.as_local.path)
+        return f'File successfully saved as {file.as_virtual.path}.'
+    except Exception as e:
+        return f'Error: {e}.'
+
+def wget_command(args: list, as_admin: bool, file_system: FileSystem) -> str:
+    if len(args) == 0:
+        return 'Please specify a url to download from.'
+    elif len(args) == 1:
+        return 'Please specify a file to download to.'
+    else:
+        url = args[0]
+        file = Path(args[1])
+
+        if os.path.exists(file.as_local.path):
+            if input('File already exists. Overwrite? [Y/n] ') == 'Y':
+                return download(url=url, file=file)
+            else:
+                return 'Download cancelled.'
+        else:
+            return download(url=url, file=file)
+
+
 module: dict = {
     'name': 'web_module',
     'description': 'Module for web-related commands.',
@@ -42,8 +69,17 @@ module: dict = {
             'description': 'Prints the contents of a website.',
             'usage': '\twprint <url> - Prints the contents of a website.',
             'needs_root': False,
-            'needs_fs': True,
+            'needs_fs': False,
             'function': wprint_command
+        },
+        'wget_command': {
+            'name': 'wget_command',
+            'keyword': 'wget',
+            'description': 'Downloads a file from a website.',
+            'usage': '\twget <url> <file> - Downloads a file from a website.',
+            'needs_root': False,
+            'needs_fs': True,
+            'function': wget_command
         }
     }
 }
